9c760aa54efbcc656b3ea5ce4a8f341f9bda34b0
lmiguelgarcia <miguel.garcia729@gmail.com>
2015-10-23 16:05:13 -0500

    Update 3s-03.c

--- pthread/3s-03.c
+++ pthread/3s-03.c
@@ -17,18 +17,23 @@ int max_threads = 0;
 
 void *count3s_thread(void *arg) {
 	int i;
+	clock_t t1, t2;
 	int length_per_thread = length/max_threads;
 	// Cast -> http://stackoverflow.com/questions/1640423/error-cast-from-void-to-int-loses-precision
 	//int id = *((int*)(&arg));
-	int id = *((int*)(&arg));
+	int id = *((int*)(arg));
+
 	int start = id * length_per_thread;
 	printf("\tThread [%d] starts [%d] length [%d]\n",id, start, length_per_thread);
 	pthread_mutex_lock(&mutex);
+	t1 = clock();
 	for (i = start; i < start + length_per_thread; i++) {
 		if (array[i] == 3) {
 			count++;
 		}
 	}
+	t2 = clock();
+	printf("[[3s-01] Elapsed time counting the number of 3s by the thread #%d is %f\n from the segment [%d] to [%d]\n",id, (((float)t2 - (float)t1) / 1000000.0F ) * 1000,start,(start + length_per_thread));
 	pthread_mutex_unlock(&mutex);
 }
 
@@ -50,7 +55,7 @@ void initialize_vector() {
 int main(int argc, char* argv[]) {
 	int i = 0;
 	int err;
-	clock_t t1, t2;
+	clock_t t1, t2,t3,t4;
 
 	if (argc == 2) {
 		max_threads = atoi(argv[1]);
@@ -71,14 +76,16 @@ int main(int argc, char* argv[]) {
 	fflush(stdout);
 	t1 = clock();
 	pthread_mutex_init(&mutex,NULL);
+	t3 = clock();
 	while (i < max_threads) {
 		err = pthread_create(&tid[i], NULL, &count3s_thread, &i);
 		if (err != 0) 
 			printf("[3s-03] Can't create a thread: [%d]\n", i);
 		else
-			printf("[3s-03] Thread created!\n");
+			printf("[3s-03] Thread #[%d] created!\n",i);
 		i++;
 	}
+	t4 = clock();
 	// https://computing.llnl.gov/tutorials/pthreads/#Joining
 	i = 0;
 	for (; i < max_threads; i++) {
@@ -96,6 +103,7 @@ int main(int argc, char* argv[]) {
 	printf("[3s-03] Count by threads %d\n", count);
 	printf("[3s-03] Double check %d\n", double_count);
 	pthread_mutex_destroy(&mutex);
+	printf("[[3s-01] Elapsed time that took to obtain the number of 3s in the whole array was %f\n", (((float)t4 - (float)t3) / 1000000.0F ) * 1000);
 	//printf("[[3s-03] Elapsed time %ld ms\n", ((double)t2 - t1) / CLOCKS_PER_SEC * 1000);
 	printf("[[3s-03] Elapsed time %f\n", (((float)t2 - (float)t1) / 1000000.0F ) * 1000);
 	pthread_exit(NULL);
