ae4b81c22d9fc1fe4b9d0d9ff8df6a749ced1203
lmiguelgarcia <miguel.garcia729@gmail.com>
2015-10-27 19:42:40 -0500

    Update pi_openmp.c

--- openmp/pi_openmp.c
+++ openmp/pi_openmp.c
@@ -5,15 +5,13 @@ static long num_steps = 10000;
 double step;
 
 void main() {
-	int i,chunk,numthreads; 
+	int i; 
 	double pi;
 	double x;
 	double sum = 0.0;
 	step = 1.0/(double) num_steps;
-	chunk=1000;
 	
-	#pragma omp parallel for private(i,x) \
-	 schedule(static, chunk) reduction(+:sum)
+	#pragma omp parallel for private(i,x) reduction(+:sum)
 	for (i = 0; i < num_steps; i++){
 		x = (i + 0.5)*step;
 		sum = sum + 4.0/(1.0 + x*x);
